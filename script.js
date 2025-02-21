d69d70a8eb76b27e218098ab20443de32eec0014
asima295 <ftasleem581@gmail.com>
2025-02-21 18:41:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,500 @@
+const quiz = [
+  /*
+  {
+  question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+  },
+  {
+   question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+  },
+   {
+   question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+  },
+  {
+   question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+
+  },
+  {
+   question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+
+  },
+  {
+   question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+
+  },
+  {
+    question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+
+  },
+  {
+    question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+  },
+  {
+   question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+  },
+  {
+    question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+  },
+  */
+  /*
+   {
+    question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+   },
+   {
+   question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+   },
+   {
+    question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+   },
+   {
+    question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+ 
+   },
+   {
+     question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+   },
+   {
+     question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+ 
+   },
+   {
+    question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+ 
+   },
+   {
+    question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+   },
+   {
+     question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+   },
+   {
+     question: "",
+    ans1text: "",
+    ans2text: "",
+    ans3text: "",
+    ans4text: "",
+    answer: "",
+   },
+   */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  {
+    question: "Which method can be used to check if a Set contains a specified value?",
+    ans1text: "contains()",
+    ans2text: "indexOf()",
+    ans3text: "has()",
+    ans4text: "inSet()",
+    answer: "has()",
+  },
+  {
+    question: "Which method returns an Iterator object with all the values in a Set?",
+    ans1text: "all()",
+    ans2text: "values()",
+    ans3text: "items()",
+    ans4text: "iterator()",
+    answer: "values()",
+  },
+  {
+    question: "What happens if you try to add a value that already exists?",
+    ans1text: "An error will occur",
+    ans2text: "The Set will not add the new value, but keep the first occurance",
+    ans3text: "The Set will add the new value and delete the first occurance",
+    ans4text: "None of the Above",
+    answer: "The Set will not add the new value, but keep the first occurance",
+  },
+  {
+    question: '<p id="demo">This is a demonstration.</p>',
+    ans1text: 'document.getElementByName("p").innerHTML = "Hello World!";',
+    ans2text: 'document.getElementByName("p").innerHTML = "Hello World!"; ',
+    ans3text: 'document.getElementById("demo").innerHTML = "Hello World!";',
+    ans4text: '#demo.innerHTML="Hello Word";',
+    answer: 'document.getElementById("demo").innerHTML = "Hello World!";',
+
+  },
+  {
+    question: "The external JavaScript file must contain the <script> tag.",
+    ans1text: "True",
+    ans2text: "False",
+    ans3text: "Both a & b",
+    ans4text: "None of the Above",
+    answer: "False",
+
+  },
+  {
+    question: 'How do you write "Hello World" in an alert box?',
+    ans1text: 'alert("Hello World");',
+    ans2text: 'msg("Hello World");',
+    ans3text: 'alertBox("Hello World");',
+    ans4text: 'msgBox("Hello World");',
+    answer: 'alert("Hello World");',
+
+  },
+  {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    ans1text: "onclick",
+    ans2text: "onmouseover",
+    ans3text: "onmouseclick",
+    ans4text: "onchange",
+    answer: "onclick",
+  },
+  {
+    question: "How do you declare a JavaScript variable?",
+    ans1text: "var carName;",
+    ans2text: "v carName;",
+    ans3text: "variable carName;",
+    ans4text: "Both a & b",
+    answer: "var carName;",
+  },
+  {
+    question: "Which operator is used to assign a value to a variable?",
+    ans1text: "+",
+    ans2text: "-",
+    ans3text: "=",
+    ans4text: "*",
+    answer: "=",
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    ans1text: "Both the <head> section and the <body> section are correct",
+    ans2text: "The <body> section",
+    ans3text: "The <head> section",
+    ans4text: "Both b & c",
+    answer: "Both the <head> section and the <body> section are correct",
+  },
+
+  {
+    question: "** equal to",
+    ans1text: "Exponentiation",
+    ans2text: "Multiplication",
+    ans3text: "	Increment",
+    ans4text: "Addition",
+    answer: "Exponentiation",
+  },
+  {
+    question: "% equal to",
+    ans1text: "Exponentiation",
+    ans2text: "Modulus",
+    ans3text: "Modulus",
+    ans4text: "Both b & c",
+    answer: "Both b & c",
+  },
+  {
+    question: "Variables created with the let keyword can never change their value.",
+    ans1text: "True",
+    ans2text: "False",
+    ans3text: "declared",
+    ans4text: "Both a & b",
+    answer: "False",
+  },
+  {
+    question: "isInteger(10.1)",
+    ans1text: "true",
+    ans2text: "false",
+    ans3text: "10",
+    ans4text: "1",
+    answer: "false",
+
+  },
+  {
+    question: "MAX_SAFE_INTEGER or MiN_SAFE_INTEGER",
+    ans1text: "16 or -16",
+    ans2text: "15 or 15",
+    ans3text: "-16",
+    ans4text: "16",
+    answer: "16 or -16",
+  },
+  {
+    question: "The typeof(Undefined)",
+    ans1text: "Null",
+    ans2text: "Undefined",
+    ans3text: "Number",
+    ans4text: "Object",
+    answer: "Undefined",
+
+  },
+  {
+    question: "If the result is negative?",
+    ans1text: "a is sorted before b",
+    ans2text: "b is sorted before a",
+    ans3text: "Both a & b",
+    ans4text: "Numeric Sort",
+    answer: "a is sorted before b",
+
+  },
+  {
+    question: "combining sort() and reverse(), you can sort an array?",
+    ans1text: "increasing order",
+    ans2text: "Alphabetic Sort",
+    ans3text: "descending order",
+    ans4text: "Numeric Sort",
+    answer: "descending order",
+  },
+  {
+    question: "What is the use of pop() method?",
+    ans1text: "removes the last element",
+    ans2text: "adds a new element",
+    ans3text: "removes the first element",
+    ans4text: "find array length",
+    answer: "removes the last element",
+  },
+  {
+    question: "Math.sign(4.7) return",
+    ans1text: "4",
+    ans2text: "5",
+    ans3text: "1",
+    ans4text: "7",
+    answer: "1",
+  },
+
+  {
+    question: "How does a For loop start?",
+    ans1text: "for(i=0; i<=5; i++)",
+    ans2text: "fori=1 to 5",
+    ans3text: "for(i=0; i<=5)",
+    ans4text: "for(i<=5; i++)",
+    answer: "for(i=0; <=5; i++)",
+  },
+  {
+    question: "Comparing two JavaScript objects always return?",
+    ans1text: "false",
+    ans2text: "true",
+    ans3text: "object",
+    ans4text: "string",
+    answer: "false",
+  },
+  {
+    question: "Math.random() returns a random number between?",
+    ans1text: "0 & 1",
+    ans2text: "1 & 10",
+    ans3text: "less then 0",
+    ans4text: "Both a & c",
+    answer: "0 & 1",
+  },
+  {
+    question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    ans1text: "if (i != 5)",
+    ans2text: "if i <> 5",
+    ans3text: "if (i <> 5)",
+    ans4text: "if i =! 5 then",
+    answer: "if (i != 5)",
+  },
+  {
+    question: "How do you find the number with the highest value of x and y?",
+    ans1text: "Math.max(x, y)",
+    ans2text: "top(x, y)",
+    ans3text: "Math.ceil(x,y)",
+    ans4text: "ceil(x,y)",
+    answer: "Math.max(x, y)",
+
+  },
+  {
+    question: "How do you round the number 7.25, to the nearest integer?",
+    ans1text: "Math.rnd(7.25)",
+    ans2text: "Math.round(7.25)  ",
+    ans3text: "round(7.25)",
+    ans4text: "rnd(7.25)",
+    answer: "Math.round(7.25)  ",
+
+  },
+  {
+    question: "How does a WHILE loop start?",
+    ans1text: "while (i <= 10; i++)",
+    ans2text: "while (i <= 10)",
+    ans3text: "while i = 1 to 10",
+    ans4text: "Both a & b",
+    answer: "while (i <= 10)",
+
+  },
+  {
+    question: "Math.ceil(4.9) return",
+    ans1text: "4",
+    ans2text: "5",
+    ans3text: "1",
+    ans4text: "9",
+    answer: "5",
+  },
+  {
+    question: 'const d = new Date("2022-03-25") return;',
+    ans1text: "Fri Mar 25 2022 05:00:00",
+    ans2text: "Fri Feb 21 2025 18:00:09",
+    ans3text: "",
+    ans4text: "None of the Above",
+    answer: "",
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    ans1text: "if i == 5 then",
+    ans2text: "if (i == 5)",
+    ans3text: "if i = 5",
+    ans4text: "if i = 5 then",
+    answer: "if (i == 5)",
+  },
+
+]
+const question = document.getElementById("quiz-question");
+console.log(question);
+console.log(question.textContent)
+const option_a = document.getElementById("text_option_a");
+const option_b = document.getElementById("text_option_b");
+const option_c = document.getElementById("text_option_c");
+const option_d = document.getElementById("text_option_d");
+const answerElement = document.querySelectorAll(".answer");
+console.log(option_a);
+console.log(option_b);
+console.log(option_c);
+console.log(option_d);
+console.log(option_a.textContent);
+console.log(option_b.textContent);
+console.log(option_c.textContent);
+console.log(option_d.textContent);
+
+const submit = document.getElementById("submit");
+
+let currentQuestion = 0;
+let score = 0;
+
+console.log(quiz[currentQuestion].question);
+console.log(quiz[currentQuestion].ans1text);
+console.log(quiz[currentQuestion].ans2text);
+console.log(quiz[currentQuestion].ans3text);
+console.log(quiz[currentQuestion].ans4text);
+
+question.textContent = quiz[currentQuestion].question;
+option_a.textContent = quiz[currentQuestion].ans1text;
+option_b.textContent = quiz[currentQuestion].ans2text;
+option_c.textContent = quiz[currentQuestion].ans3text;
+option_d.textContent = quiz[currentQuestion].ans4text;
+
+
+submit.addEventListener("click", () => {
+  const checkedAns = document.querySelector('input[type="radio"]:checked')
+  console.log(checkedAns);
+  // console.log(checkedAns.nextElementSibling.textContent);
+  if (checkedAns === null) {
+    alert("Please select an answer");
+  } else {
+    if (checkedAns.nextElementSibling.textContent === quiz[currentQuestion].answer) {
+      score++;
+    }
+
+    currentQuestion++;
+    if (currentQuestion < quiz.length) {
+      question.textContent = quiz[currentQuestion].question;
+      option_a.textContent = quiz[currentQuestion].ans1text;
+      option_b.textContent = quiz[currentQuestion].ans2text;
+      option_c.textContent = quiz[currentQuestion].ans3text;
+      option_d.textContent = quiz[currentQuestion].ans4text;
+      checkedAns.checked = false;
+    } else {
+      alert("Your score is " + score + " out of " + quiz.length);
+      location.reload();
+    }
+
+  }
+});
